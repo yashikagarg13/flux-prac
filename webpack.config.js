0a08d66b9cd38695c3c2ebe64dcdb000e18983d0
Yashika Garg <yashikagarag13@gmail.com>
2017-03-16 14:11:01 +0530

    Egghead flux prac - setup basic react component

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname) + "/dist",
+    filename: "bundle.js",
+    publicPath: "/", // For urls
+  },
+  devServer: {
+    inline: true,
+    contentBase: "./dist"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2015", "react"],
+        }
+      }
+    ]
+  }
+};
